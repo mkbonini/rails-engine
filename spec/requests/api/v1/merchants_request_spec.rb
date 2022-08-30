8d736a3febe287e3951fa827194b7e5ba55d0c60
mkbonini <mkbonini@gmail.com>
2022-08-30 14:02:20 -0600

    test: mercahnts show endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'The merchants API' do
   it 'sends a list of merchants' do
-    create_list(:mercahnts, 3)
+    create_list(:merchant, 3)
 
     get '/api/v1/merchants'
 
@@ -15,10 +15,29 @@ RSpec.describe 'The merchants API' do
 
     merchants.each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      # expect(merchant[:id]).to be_an(Integer)
 
       expect(merchant).to have_key(:attributes)
       expect(merchant[:attributes][:name]).to be_a(String)
+
+      expect(merchant[:attributes]).to_not have_key(:created_at)
     end
+  end
+
+  it 'sends a single merchant by id' do
+    merchants = create_list(:merchant, 2)
+
+    get "/api/v1/merchants/#{merchants.first.id}"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    
+    merchant = response_body[:data]
+
+    expect(response).to be_successful
+    expect(merchant).to have_key(:id)
+    expect(merchant).to have_key(:attributes)
+    expect(merchant[:attributes][:name]).to eq(merchants.first.name)
+
+    expect(merchant[:attributes][:name]).to_not eq(:merchants.last.name)
   end
 end
